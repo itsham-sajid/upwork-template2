8d7c206b9184304b402b8537ba50fa52cd07cd0d
heetobha <heetubhasbad@gmail.com>
2024-08-04 19:57:11 +0500

    create template2

---
+++ app/page.jsx
@@ -0,0 +1,36 @@
+import HeaderFive from "@/components/headers/HeaderFive";
+import PersonalInfo from "@/components/personalInfo/PersonalInfo";
+import About from "@/components/About/About";
+import Resume from "@/components/resume/Resume";
+import Portfolio from "@/components/portfolio/Portfolio"
+import Blogs from "@/components/blogs/Blogs";
+import Contact from "@/components/contact/Contact";
+
+
+
+import React from "react";
+
+export const metadata = {
+  title: "Home || Bostami - Professional portfolio NextJS Template",
+  description:
+    "Discover Botami,the most impressive portfolio template for work showcase, blog",
+};
+export default function Home() {
+  return (
+    <>
+      <div className="page-wrapper page-wrapper-2">
+        <HeaderFive />
+        <div className="bostami-page-area z-index-3">
+          <div className="container">
+            <PersonalInfo />
+            <About />
+            <Resume />
+            <Portfolio />
+            <Blogs />
+            <Contact />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
